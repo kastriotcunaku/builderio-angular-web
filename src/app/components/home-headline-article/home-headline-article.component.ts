2044629f0dabddf106f34cfa61e9129c7142c2b7
Kastriot Çunaku <kastriotcunaku@gmail.com>
2024-02-07 19:04:25 +0100

    Gallery view/close

--- src/app/components/home-headline-article/home-headline-article.component.ts
+++ src/app/components/home-headline-article/home-headline-article.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-headline-article.component.scss']
 })
 export class HomeHeadlineArticleComponent {
+  isOpen: boolean = false;
 
+  close() {
+    this.isOpen = false;
+  }
 }
