830d42dd0962b14ca96cb1d96841d182caec1dc6
Kastriot Çunaku <kastriotcunaku@gmail.com>
2024-02-07 18:28:57 +0100

    home headline article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeHeadlineArticleComponent } from './components/home-headline-article/home-headline-article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeHeadlineArticleComponent
   ],
   imports: [
     BrowserModule,
